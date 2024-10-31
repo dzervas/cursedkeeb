09aca20ebbcc2b895965105634581d246cda1224
Dimitris Zervas <dzervas@dzervas.gr>
2024-10-31 17:45:27 +0200

    Create the base keycap
    Signed-off-by: Dimitris Zervas <dzervas@dzervas.gr>

--- keycap.py
+++ keycap.py
@@ -1,20 +1,26 @@
-# %%
-
-# The markers "# %%" separate code blocks for execution (cells)
-# Press shift-enter to exectute a cell and move to next cell
-# Press ctrl-enter to exectute a cell and keep cursor at the position
-# For more details, see https://marketplace.visualstudio.com/items?itemName=ms-toolsai.jupyter
-
-# %%
-
 from build123d import *
 from ocp_vscode import *
 
-# %%
-# Builder mode
+thickness = 1.5
+walls = 2.0
+
+# Stem
+stem_height = 3.6
+stem_width = 3.8
+stem_spacing = 14.5 # internal space from stem to stem in 1U keys
+
+# Stem stepdown to catch keycap
+stem_slim_width = 3.0
+stem_slim_height = 0.8
+
+keycap_gap = 1
+single = stem_spacing + stem_width
 
 with BuildPart() as bp:
-    Box(1,1,1)
-    chamfer(bp.edges(), length=0.1)
+    Box(single, single, thickness)
+    with Locations(Vector(0, 0, -stem_height/2 - thickness/2)):
+        Box(stem_width+walls, stem_width+walls, stem_height)
+        Box(stem_width, stem_width, stem_height, mode=Mode.SUBTRACT)
 
-show(bp, axes=True, axes0=True, grid=(True, True, True))
+show(bp, axes=True, axes0=True, grid=(True, True, True), transparent=True)
+export_stl(bp, "keycap.stl")
